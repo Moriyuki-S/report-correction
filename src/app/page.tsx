d234602bdec3d2152608be4aaeb8f57ad1ffc4b7
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-10 16:36:15 +0000

    install : shadcn/uiをインストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <Button>Hello World</Button>
   );
 }
