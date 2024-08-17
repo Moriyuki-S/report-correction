eb301d174b5353610f19846416bd5efac7ebcb7f
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-17 14:14:41 +0000

    add : ReportUploadInputコンポーネントを作成

---
+++ src/feature/report/components/ReportUploadInput.tsx
@@ -0,0 +1,13 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const ReportUploadButton = () => {
+    return (
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="レポート">レポートをアップロード</Label>
+            <Input id="report" type="file" className="cursor-pointer" />
+        </div>
+    );
+};
+
+export default ReportUploadButton;
