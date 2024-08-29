b873120bca0fc14c3385216285651e0fe474fbe6
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-29 02:44:44 +0000

    fix : htmlForの値を修正

--- src/feature/report/components/report-upload-input.tsx
+++ src/feature/report/components/report-upload-input.tsx
@@ -4,7 +4,7 @@ import { Label } from "@/components/ui/label";
 const ReportUploadButton = () => {
     return (
         <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="レポート">レポートをアップロード</Label>
+            <Label htmlFor="report">レポートをアップロード</Label>
             <Input id="report" type="file" className="cursor-pointer" />
         </div>
     );
